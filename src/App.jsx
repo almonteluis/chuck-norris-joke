55b138a37d30df0d516c966075c9c388e4d0bf31
Luis A <almonteluis92@gmail.com>
2023-03-06 17:05:46 -0500

    quick react app for a random chuck norris joke at load

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Joke from "./components/Joke";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Chunk Norris Joke App</h1>
+      <Joke />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Joke.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import Norris from "/src/assets/Norris.jpg";
+import axios from "axios";
+
+const Joke = () => {
+  const [joke, setJoke] = useState(""); // This is the state for the joke
+  const [gettagline, setGetTagline] = useState(""); // This is the state for the tagline
+
+  const tagline = {
+    taglines: [
+      "When Chuck Norris tells a joke, even the punchline is afraid.",
+      "Generating Chuck Norris jokes faster than his roundhouse kicks.",
+      "Chuck Norris doesn't need a punchline, his jokes hit you before you even hear them.",
+      "Warning: May cause uncontrollable laughter and fear of Chuck Norris.",
+      "Our Chuck Norris joke generator is so good, it can even make Chuck Norris laugh.",
+      "When Chuck Norris jokes, even the crickets stop chirping to listen.",
+      "Our Chuck Norris joke generator is like a roundhouse kick to the funny bone.",
+      "If Chuck Norris had a joke generator, it would be ours.",
+      "You don't choose the Chuck Norris joke, the Chuck Norris joke chooses you.",
+      "Our Chuck Norris joke generator: the only thing scarier than a roundhouse kick to the face.",
+    ],
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://api.chucknorris.io/jokes/random")
+      .then((res) => {
+        setJoke(res.data.value);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  }, []); // This is the effect for the joke
+
+  useEffect(() => {
+    const randomTagline = Math.floor(Math.random() * tagline.taglines.length);
+    setGetTagline(tagline.taglines[randomTagline]);
+  }, [gettagline]); // This is the effect for the tagline
+
+  return (
+    <div className='container mx-auto items-center flex flex-wrap mt-5'>
+      <div className='max-w-sm w-full lg:max-w-full lg:flex'>
+        <div
+          className='h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden'
+          style={{
+            backgroundImage: `url(${Norris})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+          }}
+          title='Woman holding a mug'
+        ></div>
+        <div className='border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal'>
+          <div className='mb-8'>
+            <div className='text-gray-900 font-bold text-xl mb-2'>
+              {gettagline}
+            </div>
+            <p className='text-gray-700 text-base'>{joke}</p>
+          </div>
+        </div>
+      </div>
+      <div className='font-semibold text-1xl text-slate-500'></div>
+    </div>
+  );
+};
+
+export default Joke;
